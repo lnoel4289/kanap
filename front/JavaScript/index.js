367ba1f56c0a3b608ab9f5bf6a5fd5c5b6df826a
Laurent NOËL <lnoel4289@gmail.com>
2022-12-01 18:59:45 +0100

    premières vagues tentatives

---
+++ front/JavaScript/index.js
@@ -0,0 +1,17 @@
+/** Requéter l'API
+*/
+
+
+
+
+
+
+/** Copier le noeud 'section.items a'
+*/
+
+// function addItem() {
+//     let item = querySelector('section.items > a');
+//     let nextItem = item.cloneNode();
+//     nextItem.append;
+// };
+
