ebbd89877ec75d502fa54ed2e8391b0d7a4d29d3
Laurent NOËL <lnoel4289@gmail.com>
2022-12-02 12:25:45 +0100

    corrections mineures

---
+++ front/js/script.js
@@ -0,0 +1,25 @@
+/** Requéter l'API
+*/
+
+// fetch('http://localhost:3000/api/products')
+//     .then(function(res) {
+//         if (res.ok) {
+//             return res.json();
+//         }
+//     });
+// console.log(res.json)
+
+
+
+
+
+
+/** Copier le noeud 'section.items a'
+*/
+
+function addItem() {
+    let item = querySelector('section.items > a');
+    let nextItem = item.cloneNode();
+    nextItem.append;
+};
+
