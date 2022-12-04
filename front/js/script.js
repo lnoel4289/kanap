6c098844ecce0a80edea894d20cee082c0a8374d
Laurent NOËL <lnoel4289@gmail.com>
2022-12-04 14:15:48 +0100

    test affichage back/front

--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,38 @@
-/** Requéter l'API
-*/
-
-// fetch('http://localhost:3000/api/products')
-//     .then(function(res) {
-//         if (res.ok) {
-//             return res.json();
-//         }
-//     });
-// console.log(res.json)
-
-
-
-
-
-
 /** Copier le noeud 'section.items a'
 */
+// let container = document.getDocumentById('items')
+// let item = document.querySelector('section.items > a');
+// let nextItem = item.cloneNode();
+// container.append(nextItem);
 
-function addItem() {
-    let item = querySelector('section.items > a');
-    let nextItem = item.cloneNode();
-    nextItem.append;
-};
+
+/** Requéter l'API
+*/
+fetch('http://localhost:3000/api/products') // requête GET HTTP
+    .then(function(res) { // 
+        if (res.ok) { // si la requête s'est bien passée
+            return res.json(); // promesse
+        }
+    })
+    .then(function(value) {
+        console.log(value); // monitoring
+    })
+    .catch(function(err) {
+        console.log('error'); // monitoring en cas d'erreur (requête échouée)
+    });
+
+/**
+ * Affichage produits
+ */
+let items = document.getElementsByClassName('items');
+
+items[0].querySelector("img").src = "http://localhost:3000/kanap01.jpeg";
+items[0].querySelector("img").alt = "bonjour";
+items[0].querySelector('a > article > h3')
+    .textContent = 'bonjour';
+items[0].querySelector('a > article > p')
+    .textContent = 'bonjour';
+
+let item = items[0].cloneNode;
+item.append;
 
