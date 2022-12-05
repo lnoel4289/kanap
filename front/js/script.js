102067ffa91e66361a96cbce3633fc662d6f8a8b
Laurent NOËL <lnoel4289@gmail.com>
2022-12-05 10:25:04 +0100

    save

--- front/js/script.js
+++ front/js/script.js
@@ -1,40 +1,32 @@
+let item = document.querySelector('section.items > a');
+
+// function addItem() {
+
 /** Requéter l'API
 */
-fetch('http://localhost:3000/api/products') // requête GET HTTP
+fetch('http://localhost:3000/api/products/') // requête GET HTTP
     .then(function(res) { // 
         if (res.ok) { // si la requête s'est bien passée
             return res.json(); // promesse
         }
     })
+    /** Insertion des données de l'objet récupéré (value)
+     * 
+     */
     .then(function(value) {
-        console.log(value); // monitoring
+        item.querySelector("img").src = value[0].imageUrl;
+        item.querySelector("img").alt = value[0].altTxt;
+        item.querySelector('a > article > h3')
+            .textContent = value[0].name;
+        item.querySelector('a > article > p')
+            .textContent = value[0].description;
     })
     .catch(function(err) {
-        console.log('error'); // monitoring en cas d'erreur (requête échouée)
+        alert('Echec du serveur');
     });
+    /** Copier le noeud 'section.items a'
+    */
+    let item02 = item.cloneNode(true);
+    document.getElementById('items').appendChild(item02);
 
-
-
-/** Copier le noeud 'section.items a'
-*/
-let item01 = document.querySelector('section.items > a')
-let item02 = item01.cloneNode(true);
-document.getElementById('items').appendChild(item02);
-
-
-
-
-/**
- * Affichage produits
- */
-let item = document.querySelector('section.items > a');
-
-item.querySelector("img").src = "http://localhost:3000/kanap01.jpeg";
-item.querySelector("img").alt = "bonjour";
-item.querySelector('a > article > h3')
-    .textContent = 'bonjour';
-item.querySelector('a > article > p')
-    .textContent = 'bonjour';
-
-
-
+// }
