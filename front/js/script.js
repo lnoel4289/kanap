baf857e144b0ab44580e3c59b70b062d5f2f9b41
Laurent NOËL <lnoel4289@gmail.com>
2022-12-07 12:42:57 +0100

    Ajout id dans le lien produit (page d'accueil)

--- front/js/script.js
+++ front/js/script.js
@@ -2,14 +2,14 @@
 fetch('http://localhost:3000/api/products/')
     .then((res) => res.json())
     .then((products) => {
-    myKanaps(products);
+    productDisplay(products);
     })
     .catch((error) => {
         document.querySelector('.titles').innerHTML = '<h1>Serveur indisponible</h1>'
     });
 
 // Fonction/boucle remplissant le noeud 'item' puis le clonant
-function myKanaps(products) {
+function productDisplay(products) {
     for (product of products) {
 
         // Remplir chaque item avec les propriétés de chaque propriété de l'array products
@@ -18,12 +18,15 @@ function myKanaps(products) {
         item.querySelector('img').alt = product.altTxt;
         item.querySelector('.productName').textContent = product.name;
         item.querySelector('.productDescription').textContent = product.description;
+        item.href = './product.html?id=' + product._id;
 
         // Boucle interrompue si le nombre d'enfants de 'items' est égal au nombre de 'product' du tableau 'products'
         if (items.childElementCount == products.length) {
-        break;
+        break; // je ne comprends pas pourquoi 'items' est reconnu alors qu'il n'est pas défini
     }
         // Suite de la boucle si celle-ci n'a pas été interrompue - Copie du noeud 'item'
         document.getElementById('items').appendChild(item.cloneNode(true));
     }
 }
+
+// Je ne comprends pourquoi le dernier canapé de 'products' apparaît en premier
