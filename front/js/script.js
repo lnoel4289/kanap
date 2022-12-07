a63a20ddea0bb922056c871b50ab528cdc2f6d8d
Laurent NOËL <lnoel4289@gmail.com>
2022-12-07 11:48:39 +0100

    Affichage de la page d'accueil OK (script.js)

--- front/js/script.js
+++ front/js/script.js
@@ -1,67 +1,29 @@
-let items = document.querySelector('#items');
-let item = document.querySelector('section.items > a');
-
-// function addItem() {
-
-/** Requéter l'API
-*/
-// fetch('http://localhost:3000/api/products/') // requête GET HTTP
-//     .then(function(res) { // 
-//         if (res.ok) { // si la requête s'est bien passée
-//             return res.json(); // promesse
-//         }
-//     })
-//     /** Insertion des données de l'objet récupéré (value)
-//      * 
-//      */
-//     .then(function(value) {
-//         item.querySelector("img").src = value[0].imageUrl;
-//         item.querySelector("img").alt = value[0].altTxt;
-//         item.querySelector('a > article > h3')
-//             .textContent = value[0].name;
-//         item.querySelector('a > article > p')
-//             .textContent = value[0].description;
-//     })
-//     .catch(function(err) {
-//         alert('Echec du serveur');
-//     });
-//     /** Copier le noeud 'section.items a'
-//     */
-//     let item02 = item.cloneNode(true);
-//     document.getElementById('items').appendChild(item02);
-
-// }
-
-// chris
+//  Requêter l'API
 fetch('http://localhost:3000/api/products/')
     .then((res) => res.json())
-    .then((myProducts) => {
-    myKanaps(myProducts);
+    .then((products) => {
+    myKanaps(products);
     })
     .catch((error) => {
-        document.querySelector('.titles').innerHTML = '<h1>Server indisponible</h1>'
+        document.querySelector('.titles').innerHTML = '<h1>Serveur indisponible</h1>'
     });
 
+// Fonction/boucle remplissant le noeud 'item' puis le clonant
 function myKanaps(products) {
-    for (i = 1; i < (products.length); i++) {
+    for (product of products) {
 
-        // articlesZone.innerHTML += `
-        // <a href="./product.html?id=42">
-        // <article>
-        //     <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-        //     <h3 class="productName">Kanap name1</h3>
-        //     <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-        // </article>
-        // </a>`
+        // Remplir chaque item avec les propriétés de chaque propriété de l'array products
+        let item = document.querySelector('#items > a');
+        item.querySelector('img').src = product.imageUrl;
+        item.querySelector('img').alt = product.altTxt;
+        item.querySelector('.productName').textContent = product.name;
+        item.querySelector('.productDescription').textContent = product.description;
 
-        // La boucle sera rompue si le nombre d'enfants de 'items' est égal au nombre d'éléments du tableau
+        // Boucle interrompue si le nombre d'enfants de 'items' est égal au nombre de 'product' du tableau 'products'
         if (items.childElementCount == products.length) {
         break;
     }
-
-        // Copie du noeud 'items'
-        document.getElementById('items').appendChild(item.cloneNode(true)); 
-
-        
+        // Suite de la boucle si celle-ci n'a pas été interrompue - Copie du noeud 'item'
+        document.getElementById('items').appendChild(item.cloneNode(true));
     }
 }
