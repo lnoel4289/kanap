17fb59e5730b00c1e86b928b332369579261f938
Laurent NOËL <lnoel4289@gmail.com>
2022-12-09 12:47:00 +0100

    save

---
+++ front/js/product.js
@@ -0,0 +1,21 @@
+// Récupérer l'id du produit dans l'url
+let url = document.location.href;
+let idNumber = url.substring(url.lastIndexOf("=")+1);
+console.log (idNumber); // monitoring
+
+// Récupérer le tableau 'products' avec fetch
+
+// Comparer avec l'id présent dans les propriétés de 'products.js'
+// if (idNumber == )
+
+// stocker l'objet parent d'"_id" dans une variable
+
+// Afficher les propriétés nécessaires du 'product' sur la page product.html
+
+// Ecouter les changements sur nombre et couleur (le choix de couleur nécessitera une boucle ?for of? car le nombre de couleurs disponibles dépend de l'objet)
+
+// Définir un local storage ? Y envoyer le nombre et la couleur choisis
+
+//  Transmettre les données du local storage à la page panier (?)
+
+// END
