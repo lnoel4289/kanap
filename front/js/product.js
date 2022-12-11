05e4333b02639c6d5ae4d11f2b63b96cdf38f580
Laurent NOËL <lnoel4289@gmail.com>
2022-12-11 19:06:23 +0100

    dernier commit

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,7 @@
 // Récupérer l'id du produit dans l'url
-let urlString = document.location.href;
-let currentUrl = new URL(urlString);
-let idNumber = currentUrl.searchParams.get('id');
+let urlString = document.location.href
+let currentUrl = new URL(urlString)
+let idNumber = currentUrl.searchParams.get('id')
 
 
 // Récupérer le tableau 'products' avec l'API Fetch
@@ -10,57 +10,53 @@ fetch('http://localhost:3000/api/products/')
     .then((products) => displayProduct(products))
     .catch((error) => {
         document.getElementsByTagName('h1').textContent = error
-    });
+    })
 
 
 //  On définit la fonction a exécuter qui comprend les autres fonctions-étapes
 function displayProduct(products) {
-    let product = fetchProduct(products); //NFE?
-    displaySpecs(product);
-    displayColors(product);
+    let product = fetchProduct(products)
+    displaySpecs(product)
+    displayColors(product)
 };
 
 // Comparer l'id récupérée dans l'Url avec l'id présente dans les propriétés de 'products' de l'API et retourner la valeur
-function fetchProduct(products){
+function fetchProduct(products) {
     for (let product of products) {
         if (product._id == idNumber) {
-            return product;
-        };
-    };
-};
+            return product
+        }
+    }
+}
 
 // Afficher les informations du 'product' sur la page product.html
 function displaySpecs(product) {
-    document.querySelector('head > title').textContent = product.name;
+    document.querySelector('head > title').textContent = product.name
 
-    const item = document.querySelector('.item');
-    item.querySelector('img').src = product.imageUrl;
-    item.querySelector('img').alt = product.altTxt;
-    document.getElementById('title').textContent = product.name;
-    document.getElementById('price').textContent = product.price;
-    document.getElementById('description').textContent = product.description;
-
-    // fonction permettant de remplir le sélecteur de couleurs en fonction du nbre de couleurs
-};
+    const item = document.querySelector('.item')
+    item.querySelector('img').src = product.imageUrl
+    item.querySelector('img').alt = product.altTxt
+    document.getElementById('title').textContent = product.name
+    document.getElementById('price').textContent = product.price
+    document.getElementById('description').textContent = product.description
+}
 
 // Remove default colors
 let options = document.getElementById('colors').children;
-options[1].remove();
-options[1].remove();
+options[1].remove()
+options[1].remove()
 
 // Insert options which number is product.colors.length
 function displayColors(product) {
     let colors = product.colors;
     for (color of colors) {
-        document.getElementById('colors').appendChild(options[0].cloneNode(true));
+        document.getElementById('colors').appendChild(options[0].cloneNode(true))
     }
-};
+}
 
 // Fonction permettant de remplir les options correctement
 function fillColorSelectorOptions(options) {
-    for (option of options ) {
-        option.querySelector
-    }
+
 }
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,15 @@
-let items = document.getElementById('items');
-let item = items.querySelector('a');
+let items = document.getElementById('items')
+let item = items.querySelector('a')
 
 //  Requêter l'API
 fetch('http://localhost:3000/api/products/')
     .then((res) => res.json())
     .then((products) => {
-    displayProducts(products);
+    displayProducts(products)
     })
     .catch((error) => {
         document.querySelector('h1').textContent = 'Serveur indisponible'
-    });
+    })
 
 // Fonction/boucle remplissant le noeud 'item' puis le clonant
 function displayProducts(products) {
@@ -19,17 +19,16 @@ function displayProducts(products) {
         item.querySelector('img').alt = product.altTxt;
         item.querySelector('.productName').textContent = product.name;
         item.querySelector('.productDescription').textContent = product.description;
-        item.href = './product.html?id=' + product._id; console.log('a');
+        item.href = './product.html?id=' + product._id;
 
         // Boucle interrompue si le nombre d'enfants de 'items' est égal au nombre de 'product' du tableau 'products'
         if (items.childElementCount == products.length) {
-        break;
+        break
     }
-    //     // Suite de la boucle si celle-ci n'a pas été interrompue - Copie du noeud 'item'
-        items.appendChild(item.cloneNode(true));
+        // Suite de la boucle si celle-ci n'a pas été interrompue - Copie du noeud 'item'
+        items.appendChild(item.cloneNode(true))
     }
 }
-
 // Je ne comprends pourquoi le dernier canapé de 'products' apparaît en premier
 
 
