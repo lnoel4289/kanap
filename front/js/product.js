323bbe034826ef71589045a43e1c7a9c4ebcb9bb
Laurent NOËL <lnoel4289@gmail.com>
2022-12-11 13:00:36 +0100

    Récupération des valeurs du product

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,38 @@
 // Récupérer l'id du produit dans l'url
-let url = document.location.href;
-let idNumber = url.substring(url.lastIndexOf("=")+1);
-console.log (idNumber); // monitoring
+let urlString = document.location.href;
+let currentUrl = new URL(urlString);
+let idNumber = currentUrl.searchParams.get('id');
 
-// Récupérer le tableau 'products' avec fetch
 
-// Comparer avec l'id présent dans les propriétés de 'products.js'
-// if (idNumber == )
+// Récupérer le tableau 'products' avec l'API Fetch
+fetch('http://localhost:3000/api/products/')
+    .then((res) => res.json())
+    .then((products) => displayProduct(products))
+    .catch((error) => {
+        document.getElementsByTagName('h1').textContent = error
+    });
 
-// stocker l'objet parent d'"_id" dans une variable
+
+//  On définit la fonction a exécuter qui comprendre les autres fonctions
+function displayProduct(products) {
+    let product = fetchProduct(products);
+    displaySpecs(product);
+};
+
+// Comparer l'id récupérée dans l'Url avec l'id présente dans les propriétés de 'products' de l'API et retourner la valeur
+function fetchProduct(products){
+    for (let product of products) {
+        if (product._id == idNumber) {
+            return product;
+        };
+    };
+};
 
 // Afficher les propriétés nécessaires du 'product' sur la page product.html
+function displaySpecs(product) {
+    console.log(product); // monitor
+    
+};
 
 // Ecouter les changements sur nombre et couleur (le choix de couleur nécessitera une boucle ?for of? car le nombre de couleurs disponibles dépend de l'objet)
 
--- front/js/script.js
+++ front/js/script.js
@@ -13,7 +13,8 @@ function productDisplay(products) {
     for (product of products) {
 
         // Remplir chaque item avec les propriétés de chaque propriété de l'array products
-        let item = document.querySelector('#items > a');
+        let items = document.getElementById('items');
+        let item = items.querySelector('a');
         item.querySelector('img').src = product.imageUrl;
         item.querySelector('img').alt = product.altTxt;
         item.querySelector('.productName').textContent = product.name;
@@ -21,11 +22,11 @@ function productDisplay(products) {
         item.href = './product.html?id=' + product._id;
 
         // Boucle interrompue si le nombre d'enfants de 'items' est égal au nombre de 'product' du tableau 'products'
-        if (items.childElementCount == products.length) {
+        if (items.getElementsByTagName('a') < products.length) {
         break; // je ne comprends pas pourquoi 'items' est reconnu alors qu'il n'est pas défini
     }
         // Suite de la boucle si celle-ci n'a pas été interrompue - Copie du noeud 'item'
-        document.getElementById('items').appendChild(item.cloneNode(true));
+        items.appendChild(item.cloneNode(true));
     }
 }
 
