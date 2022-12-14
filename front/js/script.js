df4db04bf3ac3fe20291c40ae601d3ca1a35cc3d
Laurent NOËL <lnoel4289@gmail.com>
2022-12-14 14:48:35 +0100

    index.html + script.js complete

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,3 @@
-let items = document.getElementById('items')
-let item = items.querySelector('a')
-
 //  Requêter l'API
 fetch('http://localhost:3000/api/products/')
     .then((res) => res.json())
@@ -11,44 +8,14 @@ fetch('http://localhost:3000/api/products/')
         document.querySelector('h1').textContent = 'Serveur indisponible'
     })
 
-// Fonction/boucle remplissant le noeud 'item' puis le clonant
+// injecter innerHTML avec backticks et les infos récupérées
 function displayProducts(products) {
-    // querySelector
     for (let product of products) {
-        // Remplir chaque item avec les propriétés de chaque propriété de l'array products
-        item.querySelector('img').src = product.imageUrl;
-        item.querySelector('img').alt = product.altTxt;
-        item.querySelector('.productName').textContent = product.name;
-        item.querySelector('.productDescription').textContent = product.description;
-        item.href = './product.html?id=' + product._id;
-
-        // Boucle interrompue si le nombre d'enfants de 'items' est égal au nombre de 'product' du tableau 'products'
-        if (items.childElementCount == products.length) {
-        break
-    }
-        // Suite de la boucle si celle-ci n'a pas été interrompue - Copie du noeud 'item'
-        items.appendChild(item.cloneNode(true))
+        document.getElementById('items').innerHTML += `<a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>`
     }
 }
-// Je ne comprends pourquoi le dernier canapé de 'products' apparaît en premier
-
-
-// function fillItems(products) {
-//     for (product of products) {
-//         // Remplir chaque item avec les propriétés de chaque propriété de l'array products
-//         item.querySelector('img').src = product.imageUrl;
-//         item.querySelector('img').alt = product.altTxt;
-//         item.querySelector('.productName').textContent = product.name;
-//         item.querySelector('.productDescription').textContent = product.description;
-//         item.href = './product.html?id=' + product._id;
-//     }
-// }
-
-// function addItem(products) {
-//     for (product of products) {
-//         items.appendChild(item.cloneNode(true));
-//         if (items.childElementCount == products.length) {
-//             break;
-//         }
-//     }
-// }
