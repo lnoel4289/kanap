ae2ad35c01f7cf7adf50d2a7072f9a91556765fb
lnoel4289 <105934423+lnoel4289@users.noreply.github.com>
2022-12-24 19:17:44 +0100

    found product id = cart item id

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,66 +10,32 @@ if(cart == null || cart == [] || cart == undefined) {
 // Appeler Products dans l'API
 fetch('http://localhost:3000/api/products/')
   .then(res => res.json())
-  .then(res => console.log(res)) // monitor
-  .then(function() {
-    console.log('coucou')
+  .then((products) => {
+    for(let item of cart) {
+      products.find(function(product) {
+        if(product._id == item.id) {
+          displayProduct(product)
+        }
+      })
+    }
   })
-  .then(() => cart.find(elt => elt.id = 'a6ec5b49bd164d7fbe10f37b6363f9fb'))
-  .then(elt => console.log(elt))
-  .catch(() => document.querySelector('h1').textContent = 'Erreur inattendue')
 
-  
-// // test find
-// let found = cart.find(elt => elt.id = 'a6ec5b49bd164d7fbe10f37b6363f9fb')
-// console.log(found) // monitor
-
-
-// Récupérer les produits par une requête
-// fetch('http://localhost:3000/api/products/')
-// .then((res) => res.json)
-// .then((products) => {
-//   for(let item of cart) {
-//     products.find(product => 
-//     product._id == item.id);
-//     console.log('ye')
-//     displayProduct(product)
-//   }
-// })
-// .catch((err) => {
-// console.log(err)
-// })
-
-// Trouver dans les products (requête) le produit correspondant au produit du cart
-// cart vs products
-
-// Afficher chaque produits identifié ainsi (for of)
-
-// Affichage de la section panier
-
-// Fonction afficher le panier
-function displayCart() {
-
-}
-
-
-
-
-// Afficher le produit
-function displayProduct(prod) {
-  document.getElementById('cart__items').innerHTML +=`<article class="cart__item" data-id="${prod.id}" data-color="${prod.color}">
+// Afficher le produit (à revoir)
+function displayProduct(product) {
+  document.getElementById('cart__items').innerHTML +=`<article class="cart__item" data-id="${product._id}" data-color="${product.color}">
         <div class="cart__item__img">
           <img src="../images/product01.jpg" alt="Photographie d'un canapé">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__description">
             <h2>Nom du produit</h2>
-            <p>${prod.color}</p>
+            <p>${product.color}</p>
             <p>42,00 €</p>
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
               <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${prod.quantity}">
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
             </div>
             <div class="cart__item__content__settings__delete">
               <p class="deleteItem">Supprimer</p>
