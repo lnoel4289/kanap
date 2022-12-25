a285103ab36b4549244feebd8c519d30f7f2b8dc
lnoel4289 <105934423+lnoel4289@users.noreply.github.com>
2022-12-25 12:04:07 +0100

    Affichage du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,35 +7,26 @@ if(cart == null || cart == [] || cart == undefined) {
     console.log(cart) // monitor
 }
 
-// Appeler Products dans l'API
-fetch('http://localhost:3000/api/products/')
-  .then(res => res.json())
-  .then((products) => {
-    for(let item of cart) {
-      products.find(function(product) {
-        if(product._id == item.id) {
-          displayProduct(product)
-        }
-      })
-    }
-  })
-
-// Afficher le produit (à revoir)
-function displayProduct(product) {
-  document.getElementById('cart__items').innerHTML +=`<article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+// Appeler product dans l'API
+function display() {
+  for(let item of cart) {
+    fetch(`http://localhost:3000/api/products/${item.id}`)
+    .then(res => res.json())
+    .then((product) => {
+      document.getElementById('cart__items').innerHTML +=`<article class="cart__item" data-id="${product._id}" data-color="${item.color}">
         <div class="cart__item__img">
-          <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__description">
-            <h2>Nom du produit</h2>
-            <p>${product.color}</p>
-            <p>42,00 €</p>
+            <h2>${product.name}</h2>
+            <p>${item.color}</p>
+            <p>${product.price} €</p>
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
               <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
             </div>
             <div class="cart__item__content__settings__delete">
               <p class="deleteItem">Supprimer</p>
@@ -43,7 +34,10 @@ function displayProduct(product) {
           </div>
         </div>
       </article>`
+    })
+  }
 }
+display() // test
 
 // MODIFIER LE PANIER
 // Modifier la quantité
