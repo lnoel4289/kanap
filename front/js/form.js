a3acb44661a32c546e10d494a72a8025bce12fae
Laurent NOËL <lnoel4289@gmail.com>
2023-01-13 16:07:01 +0100

    corrections

--- front/js/form.js
+++ front/js/form.js
@@ -135,7 +135,7 @@ async function sendData(data) {
   })
   .then(res => res.json())
   .then(jsObj => window.location.href='../html/confirmation.html' + '?id=' + jsObj.orderId)
-  .catch(error => alert('Serveur indisponible'))
+  .catch((err) => alert(err + '\n\nServeur non-atteint ! Merci de réessayer plus tard.\nVotre panier sera conservé en attendant.'))
 };
 
 // Fonction conditionnant l'envoi de la requête POST
